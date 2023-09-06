c20d29fc6cc6f237cb1172804e8dce3b0c9a1880
Anatoly <anatoly.pashias6@gmail.com>
2023-09-06 23:45:52 +0300

    v0.1.1: improve plugin and docs, dependencies

--- README.md
+++ README.md
@@ -15,6 +15,40 @@ This react native module is a wrapper around the native Yoco SDKs for iOS and An
 - Please see the [Yoco documentation](https://developer.yoco.com/) for the latest requirements.
 - This is an Expo Module, therefore you need to install Expo in your project. Please see the [Expo documentation](https://docs.expo.dev/bare/installing-expo-modules/) for more information on this.
 
+# Installation
+
+- Install the package from npm:
+
+```bash
+npx expo install react-native-yoco
+```
+
+- Run prebuild script:
+
+```bash
+npx expo prebuild
+```
+
+- If device to be installed is Yoco device, additional dependencies are needed. If you are not installing it on a special Yoco device, please ignore this and the rest of the steps. Install expo-gradle-ext-vars:
+
+```bash
+npx expo install expo-gradle-ext-vars
+```
+
+- Add the plugin to your app.json with `yocoDevice` set to `true`.
+NOTE: the plugin will automatically be added as a plain string. Remove it and replace it with this:
+
+```json
+"plugins": [
+      [
+        "expo-gradle-ext-vars",
+        {
+          "yocoDevice": true
+        }
+      ],
+]
+```
+
 # License
 
 MIT license. For more information, see the LICENSE file.
--- app.plugin.js
+++ app.plugin.js
@@ -1,22 +1,43 @@
-const { withProjectBuildGradle } = require("expo/config-plugins");
+const { withProjectBuildGradle, withPlugins } = require("expo/config-plugins");
+
+// const ONO_SDK_VERSION = "1.23.6"
+
+// const YOCO_DEVICE_MODULES = [
+//   "com.yoco.ono.common:kozen-android:" + ONO_SDK_VERSION,
+//   "com.yoco.ono.android:kozenAndroid:" + ONO_SDK_VERSION,
+//   "com.yoco.ono.common:dspread-android:" + ONO_SDK_VERSION,
+//   "com.yoco.ono.common:miura-android:" + ONO_SDK_VERSION,
+//   "com.yoco.ono.common:datecs-android:" + ONO_SDK_VERSION,
+//   "com.yoco.ono.android:dspreadAndroid:" + ONO_SDK_VERSION,
+//   "com.yoco.ono.android:miuraAndroid:" + ONO_SDK_VERSION,
+//   "com.yoco.ono.android:datecsAndroid:" + ONO_SDK_VERSION,
+// ]
 
 /**
  *
  * @param {import("@expo/config-types").ExpoConfig} config
+ * @param {"default" | "device"} buildType
  */
-const withYocoSDK = (config) => {
+const androidPlugin = (config) => {
   return withProjectBuildGradle(config, (config) => {
-    const pattern = /allprojects\s*{(?:\s|\S)*?repositories(?:\s|\S){\s/;
+    const allProjectsRepositoriesPattern =
+      /allprojects\s*{(?:\s|\S)*?repositories(?:\s|\S){\s/;
     const yocoDependenciesUrlString =
       "maven { url 'https://yocotechnologies.jfrog.io/artifactory/public/' }\n";
 
-    config.modResults.contents = config.modResults.contents.replace(
-      pattern,
-      (match) => `${match}${" ".repeat(2 * 4)}${yocoDependenciesUrlString}\n`
-    );
+    if (!config.modResults.contents.includes(yocoDependenciesUrlString)) {
+      config.modResults.contents = config.modResults.contents.replace(
+        allProjectsRepositoriesPattern,
+        (match) => `${match}${" ".repeat(2 * 4)}${yocoDependenciesUrlString}\n`
+      );
+    }
 
     return config;
   });
+};
+
+const withYocoSDK = (config) => {
+  return withPlugins(config, [androidPlugin]);
 };
 
 module.exports = withYocoSDK;
