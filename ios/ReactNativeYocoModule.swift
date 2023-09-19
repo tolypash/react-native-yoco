2d6235177a63fec6028b8ffe191029be10eae0db
vsnaichuk <v.snaichuk@gmail.com>
2023-09-19 12:27:35 +0300

    feat: pairTerminal function

--- ios/ReactNativeYocoModule.swift
+++ ios/ReactNativeYocoModule.swift
@@ -5,25 +5,27 @@ public class ReactNativeYocoModule: Module {
   public func definition() -> ModuleDefinition {
     // Sets the name of the module that JavaScript code will use to refer to the module.
     Name("ReactNativeYoco")
-      
+
     Function("initialise") {
-        DispatchQueue.main.async { () -> Void in
+        DispatchQueue.main.async {
             Yoco.initialise()
         }
     }
 
     Function("configure") { (secret: String) in
-        DispatchQueue.main.async { () -> Void in
+        DispatchQueue.main.async {
             Yoco.configure(secret: secret, loggingEnabled: true, environment: .production)
         }
     }
 
     Function("getDeviceType") {
-        
+
     }
 
     AsyncFunction("pairTerminal") {
-       
+        DispatchQueue.main.async {
+            Yoco.pairTerminal()
+        }
     }
 
     AsyncFunction("charge") {
